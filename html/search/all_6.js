168b4026544490aede4c70217b7e520241e67414
Travis CI <travis@travis-ci.org>
2019-10-07 23:17:35 +0000

    Deploy code docs to GitHub Pages Travis build: 10
    Commit: 76953dacf20d70723181eb712daec05e6aa01a04

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['readals',['readALS',['../class_adafruit___v_e_m_l7700.html#aa55ce8b30bbbef50f8760b5e8d3a65df',1,'Adafruit_VEML7700']]],
   ['readlux',['readLux',['../class_adafruit___v_e_m_l7700.html#a10b7f2bdc2c78c3158f775a8cc3201c1',1,'Adafruit_VEML7700']]],
-  ['readwhite',['readWhite',['../class_adafruit___v_e_m_l7700.html#aa18efd32f5c332c1bcd446ae9f8bdbc4',1,'Adafruit_VEML7700']]]
+  ['readluxnormalized',['readLuxNormalized',['../class_adafruit___v_e_m_l7700.html#af5e93619785a19afa8357908da79769a',1,'Adafruit_VEML7700']]],
+  ['readwhite',['readWhite',['../class_adafruit___v_e_m_l7700.html#aa18efd32f5c332c1bcd446ae9f8bdbc4',1,'Adafruit_VEML7700']]],
+  ['readwhitenormalized',['readWhiteNormalized',['../class_adafruit___v_e_m_l7700.html#a11147781add947b93746422039026e40',1,'Adafruit_VEML7700']]]
 ];
