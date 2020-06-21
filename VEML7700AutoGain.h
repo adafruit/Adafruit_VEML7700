6950f18ffb6a0bbc1b7e89767816b348c4393dba
Yichi Yang <yichiyan@usc.edu>
2020-06-21 21:44:44 +0800

    Add auto gain functionality

---
+++ VEML7700AutoGain.h
@@ -0,0 +1,50 @@
+#ifndef VEML7700_AUTO_GAIN_H
+#define VEML7700_AUTO_GAIN_H
+
+#include "Adafruit_VEML7700.h"
+#include <Wire.h>
+
+#define VEML7700_GAIN_MIN VEML7700_GAIN_1_8
+#define VEML7700_GAIN_MAX VEML7700_GAIN_2
+
+#define VEML7700_IT_MAX VEML7700_IT_800MS
+#define VEML7700_IT_MIN VEML7700_IT_25MS
+
+enum VemlResultType
+{
+    GOOD,
+    TOO_LOW,
+    TOO_HIGH
+};
+
+/**
+ * @brief val: ALS reading in lux. type: if the ALS reading is good, too dark, or too bright.
+ * 
+ */
+struct VemlAutoResult
+{
+    float val;
+    VemlResultType type;
+};
+
+class VEML7700AutoGain : public Adafruit_VEML7700
+{
+public:
+    boolean begin(TwoWire *theWire = &Wire);
+    void setIntegrationTime(uint8_t it);
+    void setGain(uint8_t gain);
+    void setAutoThreshold(uint16_t low, uint16_t high);
+    uint16_t getIntegrationMs();
+    VemlAutoResult readLuxAuto(bool apply_correction = true);
+
+private:
+    uint8_t veml_it, veml_gain;
+    uint16_t low_threshold, high_threshold;
+    static uint8_t prevIt(uint8_t it);
+    static uint8_t nextIt(uint8_t it);
+    static uint8_t prevGain(uint8_t gain);
+    static uint8_t nextGain(uint8_t gain);
+    float lazyNormalize(float value);
+};
+
+#endif
