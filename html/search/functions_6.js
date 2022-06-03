1a6a51da81516e790b4d1982ce2c5db0841117c7
Doxygen CI <ci-arduino@invalid>
2022-06-03 16:09:00 +0000

    Deploy docs to GitHub Pages from commit a74120cc82
    Commit: a74120cc8236e73955e8af3bc9789f27c1ab31f3
    GitHub Actions run: 2435730062

--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['readals',['readALS',['../class_adafruit___v_e_m_l7700.html#aa55ce8b30bbbef50f8760b5e8d3a65df',1,'Adafruit_VEML7700']]],
+  ['readals',['readALS',['../class_adafruit___v_e_m_l7700.html#a50d2e811c614e1e02c12b9a8becb8396',1,'Adafruit_VEML7700']]],
   ['readlux',['readLux',['../class_adafruit___v_e_m_l7700.html#a73c222cb6ed12584655318189f1bf4e5',1,'Adafruit_VEML7700']]],
-  ['readwhite',['readWhite',['../class_adafruit___v_e_m_l7700.html#ad0da72a34edb69d7b6f439ce9be43031',1,'Adafruit_VEML7700']]]
+  ['readwhite',['readWhite',['../class_adafruit___v_e_m_l7700.html#af8f9ad598a8d730126a3305cb57738b8',1,'Adafruit_VEML7700']]]
 ];
