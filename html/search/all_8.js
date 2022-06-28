9c91699a531308b74d8d9a8ed04ecd36add999cf
Doxygen CI <ci-arduino@invalid>
2022-06-28 21:01:49 +0000

    Deploy docs to GitHub Pages from commit e0e7671050
    Commit: e0e76710508fd7cef403d46e1f06ef3301b8cabc
    GitHub Actions run: 2579095225

--- html/search/all_8.js
+++ html/search/all_8.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['setgain',['setGain',['../class_adafruit___v_e_m_l7700.html#ad4c26f81e1a4ddf83b703b677791e074',1,'Adafruit_VEML7700']]],
   ['sethighthreshold',['setHighThreshold',['../class_adafruit___v_e_m_l7700.html#a482fab8ea28ea4b31204981081dc2803',1,'Adafruit_VEML7700']]],
-  ['setintegrationtime',['setIntegrationTime',['../class_adafruit___v_e_m_l7700.html#a22030afb0ea9bc182f60c3e9d1b045b4',1,'Adafruit_VEML7700']]],
+  ['setintegrationtime',['setIntegrationTime',['../class_adafruit___v_e_m_l7700.html#a99840c86bb5e6d901f2fa2374ba70b29',1,'Adafruit_VEML7700']]],
   ['setlowthreshold',['setLowThreshold',['../class_adafruit___v_e_m_l7700.html#a30b9f757e8f50811b2bb2d9c433a7121',1,'Adafruit_VEML7700']]],
   ['setpersistence',['setPersistence',['../class_adafruit___v_e_m_l7700.html#aa0b027a336f18f172612e38d05805fb2',1,'Adafruit_VEML7700']]],
   ['setpowersavemode',['setPowerSaveMode',['../class_adafruit___v_e_m_l7700.html#ac270dde09b080b990b3c1d09356b7380',1,'Adafruit_VEML7700']]]
